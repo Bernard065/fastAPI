8fcdc8a24fd243ed6a507f8a8fbfdfa03f310cd9
+ <+>
2023-07-16 06:51:32 +0300

    creating fastAPI instance

---
+++ main.py
@@ -0,0 +1,18 @@
+# Import the fastAPI class from the fastapi module
+from fastapi import FastAPI
+
+# Create an instance of the fastAPI class
+app = FastAPI()
+
+
+# Define an API endpoint for HTTP GET requests at the root URL '/'
+# The function is asynchronous (async) and will be executed when the endpoint is requested
+@app.get("/")
+async def read_root():
+    # Return a JSON response with the message {"Hello": "World"}
+    return {"Hello": "World"}
+
+# Define another API endpoint for HTTP GET requests at "/favicon.ico" URL
+@app.get("/favicon.ico")
+async def get_favicon():
+    return {"favicon": "This is a custom favicon response"}
